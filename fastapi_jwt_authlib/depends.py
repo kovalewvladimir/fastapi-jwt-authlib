039580511228423aaa0aa34c6a86a696c036bf60
kovalewvladimir <kovalew.vladimir@gmail.com>
2025-02-13 09:04:14 +0300

    Упрощение зависимостей AuthAccess и AuthRefresh в depends.py

--- fastapi_jwt_authlib/depends.py
+++ fastapi_jwt_authlib/depends.py
@@ -4,6 +4,9 @@ from fastapi import Depends
 
 from fastapi_jwt_authlib.auth import AuthContext, AuthData, AuthJWT
 
+AuthAccess = AuthContext("access")
+AuthRefresh = AuthContext("refresh")
+
 AuthDepends = Annotated[AuthJWT, Depends(AuthJWT)]
-AuthAccessDepends = Annotated[AuthData, Depends(AuthContext("access"))]
-AuthRefreshDepends = Annotated[AuthData, Depends(AuthContext("refresh"))]
+AuthAccessDepends = Annotated[AuthData, Depends(AuthAccess)]
+AuthRefreshDepends = Annotated[AuthData, Depends(AuthRefresh)]
